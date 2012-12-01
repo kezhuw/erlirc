d3563940039914d1da5d34c9bc64a850867be3d6
Kezhu Wang <kezhuw@gmail.com>
2012-12-01 13:45:33 +0800

    delete all source files

--- erlirc_define.hrl
+++
@@ -1,5 +0,0 @@
--define(NEWSOCKET(Socket), {newsocket, Socket}).
--define(DELSOCKET(Handler), {delsocket, Handler}).
-
--define(NEWCLIENT(Nickname, Socket), {newclient, Nickname, Socket}).
--define(DELCLIENT(Nickname, Handler), {delclient, Nickname, Handler}).
--- erlirc_record.hrl
+++
@@ -1,6 +0,0 @@
--record(userinf, {
-	username,
-	realname,
-	hostname,
-	channels = []
-	}).
