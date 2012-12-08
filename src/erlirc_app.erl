9488baa7a08dc45f57fa7bb053cfd4a8c6651696
Kezhu Wang <kezhuw@gmail.com>
2012-12-08 23:52:39 +0800

    remove debug code

--- src/erlirc_app.erl
+++ src/erlirc_app.erl
@@ -7,9 +7,7 @@ start(_Type, _StartArgs) ->
     Env = application:get_all_env(),
     Ports = proplists:get_value(ports, Env, [6667]),
     NAcceptor = proplists:get_value(nacceptor, Env, 20),
-    Ret = erlirc_sup:start_link([{ports, Ports}, {nacceptor, NAcceptor}]),
-    io:format("erlirc_sup result: ~p~n", [Ret]),
-    Ret.
+    erlirc_sup:start_link([{ports, Ports}, {nacceptor, NAcceptor}]).
 
 stop(_State) ->
     ok.
--- src/erlirc_listener.erl
+++ src/erlirc_listener.erl
@@ -8,9 +8,7 @@ start_link(Port, NAcceptor) ->
     supervisor:start_link(?MODULE, [Port, NAcceptor]).
 
 init([Port, NAcceptor]) ->
-    io:format("Try listen: ~p~n", [Port]),
     {ok, Listener} = gen_tcp:listen(Port, [{active, false}]),
-    io:format("Listening on: ~p~n", [Port]),
     Acceptors = [acceptor_spec(Listener, Port, I)
         || I <- lists:seq(1, NAcceptor)],
     {ok, {{one_for_one, 30, 3600}, Acceptors}}.
